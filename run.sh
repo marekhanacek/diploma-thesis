da47850ed12fbb7d31aaf68f0a76b6e37df44255
Marek Hanáček <marekhanacek1@gmail.com>
2017-04-21 11:15:39 +0200

    v1

--- run.sh
+++ run.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
-pdflatex DP_Hanacek_Marek_2017.tex
-pdflatex DP_Hanacek_Marek_2017.tex
+pdflatex --shell-escape DP_Hanacek_Marek_2017.tex
+bibtex DP_Hanacek_Marek_2017
+pdflatex --shell-escape DP_Hanacek_Marek_2017.tex
+pdflatex --shell-escape DP_Hanacek_Marek_2017.tex
 
 rm DP_Hanacek_Marek_2017.aux
 rm DP_Hanacek_Marek_2017.bbl
@@ -9,8 +11,9 @@ rm DP_Hanacek_Marek_2017.blg
 rm DP_Hanacek_Marek_2017.lof
 rm DP_Hanacek_Marek_2017.log
 rm DP_Hanacek_Marek_2017.lot
+rm DP_Hanacek_Marek_2017.lol
 rm DP_Hanacek_Marek_2017.out
 rm DP_Hanacek_Marek_2017.pyg
 rm DP_Hanacek_Marek_2017.toc
 
-evince DP_Hanacek_Marek_2017.pdf
+evince DP_Hanacek_Marek_2017.pdf &
